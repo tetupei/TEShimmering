ef097650e7a0323253e76ab0c6680bc5c1b82e90
Teppei Eda <teppei.e5349@gmail.com>
2015-10-31 11:49:20 +0900

    Initial commit

---
+++ TEShimmering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TEShimmering
+//
+//  Created by teppeieda on 10/31/15.
+//  Copyright © 2015 teppeieda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
